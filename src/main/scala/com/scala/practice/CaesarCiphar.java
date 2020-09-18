40074471d5dcc42e01e0b715dca8638ae4f1fc3c
srikanthrao242 <srikanthraomca@yahoo.com>
2020-09-19 04:11:05 +0530

    practice for sorting some string

--- src/main/scala/com/scala/practice/CaesarCiphar.java
+++ src/main/scala/com/scala/practice/CaesarCiphar.java
@@ -1,6 +1,8 @@
 package com.scala.practice;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 import java.util.stream.Collectors;
@@ -225,9 +227,180 @@ public class CaesarCiphar {
         return result.toString();
     }
 
+    static int anagram(String s) {
+        final int mid = s.length() / 2;
+        String[] parts = {s.substring(0, mid),s.substring(mid)};
+        if(parts[0].length() != parts[1].length()){
+            return -1;
+        }
+        int count = 0;
+        for (char c : parts[0].toCharArray()){
+            if(!parts[1].contains(c+"")){
+                count ++;
+            }else{
+                parts[1] = parts[1].replaceFirst(c+"","");
+            }
+        }
+        return count;
+    }
+
+    static String twoStrings(String s1, String s2) {
+        for(char c : "abcdefghijklmnopqrstuvwxyz".toCharArray())
+        {
+            if(s1.indexOf(c) > -1 && s2.indexOf(c) > -1){
+                return "YES";
+            }
+        }
+        return "NO";
+
+    }
+
+    static int stringConstruction(String s) {
+        StringBuilder p = new StringBuilder();
+        int dollars = 0;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            String subStr = s.substring(i);
+            if(p.indexOf(subStr) != -1){
+                i = i+ p.length();
+            }else if(p.indexOf(c+"") == -1){
+                p.append(c);
+                dollars ++;
+            }
+        }
+        return (int)s.chars().distinct().count();
+    }
+
+    static String[] bigSorting(String[] unsorted) {
+        Arrays.sort(unsorted, (x, y) -> x.length() == y.length() ? x.compareTo(y) : Integer.compare(x.length(), y.length()));
+        return unsorted;
+
+    }
+
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+
+
+    static void insertionSort1(int n, int[] ar) {
+        int sort = ar[n - 1];
+        int i;
+        for (i = n - 2; (i >= 0) && (ar[i] > sort); i--) {
+            ar[i + 1] = ar[i];
+            printArray(ar);
+        }
+        ar[i + 1] = sort;
+        printArray(ar);
+    }
+
+    static void insertionSort2(int n, int[] arr) {
+        for(int i =0; i< n-1; i++){
+            int j = i;
+            while (j >= 0){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+                j--;
+            }
+
+            printArray(arr);
+        }
+
+
+    }
+
+    static int runningTime(int[] arr) {
+        int n = arr.length;
+        int runningTime = 0;
+        for(int i =0; i< n-1; i++){
+            int j = i;
+            while (j >= 0){
+                if(arr[j] > arr[j+1]){
+                    runningTime ++;
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+                j--;
+            }
+        }
+        return runningTime;
+    }
+
+    static int[] quickSort(int[] arr) {
+        int pivot=arr[0];
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            if(pivot>arr[i]){
+                for(int j=i;j>0;j--){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+            }
+        }
+        return arr;
+
+    }
+
+    static void quickSort2(int[] arr) {
+        int pivot=arr[0];
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            if(pivot>arr[i]){
+                for(int j=i;j>0;j--){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+                int[] left = Arrays.copyOfRange(arr, 0, i);
+                int[] right = Arrays.copyOfRange(arr, i + 1, n);
+                quickSort2(left);
+                quickSort2(right);
+            }
+            printArray(arr);
+        }
+
+    }
+
+    static int partition(int[] A, int lo, int hi){
+        int pivot = A[hi];
+        int i = lo;
+        for(int j =lo; j< hi; j++){
+            if(A[j] < pivot){
+                int temp = A[j];
+                A[j] = A[i];
+                A[i] = temp;
+                i ++;
+            }
+        }
+        int temp = A[hi];
+        A[hi] = A[i];
+        A[i] = temp;
+        return i;
+    }
+
+    static void quickSortInPlace(int[] A, int lo,int hi){
+        if(lo < hi){
+            int p = partition(A, lo, hi);
+            printArray(A);
+            quickSortInPlace(A, lo, p-1);
+            quickSortInPlace(A, p+1, hi);
+        }
+    }
+
+
     public static void main(String[] args){
         //weightedUniformStrings("abccddde", new int[]{6,1,3,12,5,9,10});
-        System.out.println(funnyString("bcxz"));
+        int[] A = new int[]{5, 8, 1, 3, 7, 9, 2};
+        quickSortInPlace(A, 0, A.length-1);
     }
 
 }
